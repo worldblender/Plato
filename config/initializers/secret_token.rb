4d2ff78e4c5a9c8112817d3a41c6e4b544fef203
Peter Schlichting <pete@worldblender.com>
2010-10-20 23:59:15 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plato::Application.config.secret_token = '3ff2fb4ea4a2d0a03cfcce170353020d116d3ace598aecfa485822ad848ff7e056078b51ae67e2f217f08d294e446361c135b840ddc1d7fab23c35014866848d'
