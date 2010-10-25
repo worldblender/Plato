9696bcf5398436d0296a3c07476229a6b4089d08
Peter Schlichting <pete@worldblender.com>
2010-10-25 15:44:04 -0700

    shortening the bytes

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -8,8 +8,8 @@ class GameController < ApplicationController
     if current_user.bomb_id == nil && current_user.deadtime == nil
       bomb = Bomb.new
       bomb.createtime = DateTime.now
-      bomb.latitude = params[:latitude]
-      bomb.longitude = params[:longitude]
+      bomb.latitude = params[:lat]
+      bomb.longitude = params[:lng]
       bomb.detonatetime = nil
       current_user.bomb_id = @bomb.id
       bomb.save
@@ -18,8 +18,8 @@ class GameController < ApplicationController
   end
 
   def movedPlayer
-    current_user.latitude = params[:latitude]
-    current_user.longitude = params[:longitude]
+    current_user.latitude = params[:lat]
+    current_user.longitude = params[:lng]
     current_user.save
   end
 end
