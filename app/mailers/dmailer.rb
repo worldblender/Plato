2296f7bf0d5c793624fee52965a7b9574e26d3ba
Peter Schlichting <pete@worldblender.com>
2010-10-26 16:30:15 -0700

    some attempts to get email to work

--- app/mailers/dmailer.rb
+++ app/mailers/dmailer.rb
@@ -2,9 +2,11 @@ class Dmailer < ActionMailer::Base
   default :from => "WorldBlender Founders <founders@worldblender.com"
 
   def send_text(message,user)
+    @message = message
     recipients "#{user.name} <#{user.email}>"
+    from "WorldBlender Founders <founders@worldblender.com"
     subject "[plato] New game notification"
     sent_on Time.now
-    body message
+    body :user => user
   end
 end
