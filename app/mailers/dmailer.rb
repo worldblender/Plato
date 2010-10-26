42cc8e41487936dc072c14ee86ae8deccdd731c4
Peter Schlichting <pete@worldblender.com>
2010-10-26 16:38:51 -0700

    some fixes to email/verbose failures

--- app/mailers/dmailer.rb
+++ app/mailers/dmailer.rb
@@ -4,7 +4,7 @@ class Dmailer < ActionMailer::Base
   def send_text(message,user)
     @message = message
     recipients "#{user.name} <#{user.email}>"
-    from "WorldBlender Founders <founders@worldblender.com"
+    from "WorldBlender Founders <founders@worldblender.com>"
     subject "[plato] New game notification"
     sent_on Time.now
     body :user => user
