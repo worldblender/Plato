eb638ba39364013f58322a0c3a7699769f332605
Jeff Huang <jeff@worldblender.com>
2010-10-27 15:12:31 -0700

    add bomb distance client function

--- app/helpers/bombs_helper.rb
+++ app/helpers/bombs_helper.rb
@@ -1,6 +1,7 @@
 module BombsHelper
   VELOCITY = 2.08333333 # the number of meters traveled per second, so 500 meters covered in 30 seconds => 1000 m/min = 16.67 m/s
   ACCELERATION = 0.00868055554 #  accel = meter/second/second, this means that in 60 seconds is will have increased m/s by 16.67 m/s if a=.2778
+
   def serverCalcDuration(distance)
     time = (-VELOCITY+Math.sqrt(VELOCITY*VELOCITY+2*ACCELERATION*distance))/(ACCELERATION)
     time = time / 10 # TODO(jeff): remove this for the real game, this is for debugging
@@ -9,6 +10,9 @@ module BombsHelper
 
   def clientCalcDuration
     # this should return a function which is the 'duration' function so it can be called in javascript from the client
+    timeStr = "(-#{VELOCITY}+Math.sqrt(#{VELOCITY}*#{VELOCITY}+2*#{ACCELERATION}*distance))/(#{ACCELERATION})"
+    timeStr = time + "/10" # TODO(jeff): remove this for the real game, this is for debugging
+    return timeStr
   end
 
   def damageFor(distance)
