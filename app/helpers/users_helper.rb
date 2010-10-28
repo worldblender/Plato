335e486ddceada517003d5a781f85ed796ea30a4
Peter Schlichting <pete@worldblender.com>
2010-10-27 23:04:57 -0700

    some change

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def scoreChartScore
+    score = 0
+    if(self.top_score != nil)
+      score = u.top_score
+    end
+    if(self.curScore > score)
+      score = curScore
+    end
 end
