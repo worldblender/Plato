022b7df41117db908994eaade8c365e7be3bf21c
Peter Schlichting <pete@worldblender.com>
2010-10-28 17:08:30 -0700

    move game name to a constant and change email title

--- app/mailers/dmailer.rb
+++ app/mailers/dmailer.rb
@@ -1,11 +1,11 @@
 class Dmailer < ActionMailer::Base
   default :from => "WorldBlender Founders <founders@worldblender.com"
 
-  def send_text(message,user)
+  def send_text(message,user,title)
     @message = message
     recipients "#{user.name} <#{user.email}>"
-    from "WorldBlender Founders <founders@worldblender.com>"
-    subject "[plato] New game notification"
+    from "WorldBlender <customer-service@worldblender.com>"
+    subject title
     sent_on Time.now
     body :user => user
   end
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -3,6 +3,7 @@ BOMB_TIME = 30 # seconds to explode a bomb
 RAD_PER_DEG = 0.017453293  # pi/180
 Rkm = 6371
 Rmeters = Rkm * 1000     # radius in meters
+GAME_NAME = 'Plato'
 
 #Twillio tokens
 ACCOUNT_SID="ACdfa5b2a39791a3e2e85ab40ada543f32"
