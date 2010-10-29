3cf9ea72adeac37d22651baab35f9d3a920dc66d
Peter Schlichting <pete@worldblender.com>
2010-10-28 19:07:29 -0700

    trying to fix dmailer

--- app/mailers/dmailer.rb
+++ app/mailers/dmailer.rb
@@ -1,7 +1,8 @@
 class Dmailer < ActionMailer::Base
   default :from => "WorldBlender Founders <founders@worldblender.com"
 
-  def send_text(message,user,title)
+  def send_text(message,user_id,title)
+    user = User.find(user_id)
     @message = message
     recipients "#{user.name} <#{user.email}>"
     from "WorldBlender <customer-service@worldblender.com>"
