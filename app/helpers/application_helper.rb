8a4fbd3561301dc7b6bbba45614189e6cf0d7149
Peter Schlichting <pete@worldblender.com>
2010-11-01 18:44:03 -0700

    attempted fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,6 @@
 module ApplicationHelper
   def event(params)
     event_type = EventType.get_type(params[:type])
-    event = Event.new(params)
-    event.event_type=event_type.id
-    event.save
+    Event.new(:data => params[:data], :event_type => event_type).save
   end
 end
